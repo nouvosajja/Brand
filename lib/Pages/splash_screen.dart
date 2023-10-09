113aabb16ba55fa5af06be410f450cc13be4f0f6
Andra <abhinayaandra@gmail.com>
2023-10-09 15:28:32 +0700

    Fix: [AAS] Fixed Splash Screen

--- lib/Pages/splash_screen.dart
+++ lib/Pages/splash_screen.dart
@@ -8,24 +8,53 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      SvgPicture.asset('assets/images/BG1.svg'),
-      BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
-        child: Container(
-          color: Colors.black.withOpacity(0.2),
-        ),
-      ),
-      Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, 
-              children: [
-                SvgPicture.asset('assets/images/Logo.svg'),
-                Text('Brand', style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold))
-          ]),
-        ),
-      )
-    ]);
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final screenHeight = constraints.maxHeight;
+        final screenWidth = constraints.maxWidth;
+
+        return Stack(
+          children: [
+            Container(
+              width: screenWidth,
+              height: screenHeight,
+              child: SvgPicture.asset(
+                'assets/images/BG1.svg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 30, sigmaY: 30),
+              child: Container(
+                color: Colors.black.withOpacity(0.2),
+                width: screenWidth,
+                height: screenHeight,
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/Logo.svg',
+                    height: screenHeight * 0.2,
+                  ),
+                  SizedBox(height: screenHeight * 0.03),
+                  Text(
+                    'Brand',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenHeight * 0.04,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.none,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
