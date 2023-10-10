392db43375386b9390deef54a5e1b6a89eae9ed6
Andra <abhinayaandra@gmail.com>
2023-10-10 14:47:07 +0700

    Feat: [AAS] Make Forget Password Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:brand/Login/screen.dart';
 import 'package:brand/Pages/Profile/screen.dart';
+import 'package:brand/Pages/forgetPass/forgetPass.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProfileScreen(),
+      home: forgetPass(),
     );
   }
 }
